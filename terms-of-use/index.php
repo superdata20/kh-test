6eb5d100b61dda87936c830521b23ff7fa7df2a6
Samir <web48@bk.ru>
2023-01-02 22:44:42 +0300

    content page

---
+++ terms-of-use/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../inc/header.php";
+?>
+<main class="c-page">
+    <figure class="c-diamonds-wrap cc-purple"></figure>
+    <section class="c-solution_hero-section">
+        <div class="c-container">
+            <div>
+                <div>
+                    <div class="c-title-wrapper cc-gradient-blue-1 cc-clip-text cc-margin-top">
+                        <h1 class="c-title-2">Keyholder – Terms of Use</h1>
+                    </div>
+                    <div class="c-hero-sub">
+                        <p class="c-title-4">Title ...</p>
+                        <p>text</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php
+require_once "../inc/footer.php";
+?>
