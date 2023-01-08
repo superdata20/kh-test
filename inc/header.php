94a7a661b833051cebaaac63399b943fb7234532
Samir <web48@bk.ru>
2023-01-08 15:21:09 +0300

    GTM added

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TSDTQWB');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8" />
     <title>KeyHolder: Online Rental Application & Tenant Screening</title>
     <meta content="Easily manage the entire rental process online for free with KeyHolder—from rental applications and tenant screening to renters insurance and rent payment." name="description" />
@@ -30,6 +33,9 @@
     </style>
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSDTQWB" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="c-custom-code w-embed w-iframe">
     <style>
         body {
